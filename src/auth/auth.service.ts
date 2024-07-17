10051628491d41d7678ad6d6c3e3a53f18d3022a
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-17 16:27:43 -0600

    feat: DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { CreateAuthDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()

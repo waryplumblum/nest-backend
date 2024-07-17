060f9a5b2d5412a2342b02822958a2b91d95907c
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-17 15:05:36 -0600

    feat: Environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule
   ],
   controllers: [],
